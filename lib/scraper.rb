9a3b6f22a3fd5b12413fee0dd4a793a6634259c1
shen huang <shenyh33@gmail.com>
2020-11-26 22:03:50 -1000

    commit tree ec1e3bfd9b721ed8375b98b8c208571498cb8ea1

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)html = open("https://flatironschool.com/")
+doc.css(".site-header__hero__headline") 
+doc.css(".tout__label.heading.heading--level-4") 
+
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course|
+  puts course.text.strip
+end 
